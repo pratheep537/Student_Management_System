ef164f0705c3143459d5f311b48d5b701352fed4
pratheep537 <pratheep.s537@gmail.com>
2025-12-19 09:50:03 +0530

    Update StudentService.java

--- StudentService.java
+++ StudentService.java
@@ -11,29 +11,44 @@ public class StudentService {
         int id = sc.nextInt();
         sc.nextLine();
 
+        for (Student s : students) {
+            if (s.getId() == id) {
+                System.out.println("Student with this ID already exists.");
+                return;
+            }
+        }
+
         System.out.print("Enter Name: ");
         String name = sc.nextLine();
 
         System.out.print("Enter Department: ");
-        String dept = sc.nextLine();
+        String department = sc.nextLine();
 
         System.out.print("Enter Marks: ");
         double marks = sc.nextDouble();
 
-        students.add(new Student(id, name, dept, marks));
+        students.add(new Student(id, name, department, marks));
         System.out.println("Student added successfully!");
     }
 
     public void viewStudents() {
         if (students.isEmpty()) {
-            System.out.println("No students found.");
-            return;
+             System.out.println("No students found.");
+        return;
         }
+
+        System.out.printf("%-5s %-15s %-15s %-10s%n", 
+                      "ID", "NAME", "DEPARTMENT", "MARKS");
+        System.out.println("---------------------------------------------");
+
         for (Student s : students) {
-            System.out.println(s);
+             System.out.printf("%-5d %-15s %-15s %-10.2f%n",
+                          s.getId(),
+                          s.getName(),
+                          s.getDepartment(),
+                          s.getMarks());
         }
     }
-
     public void searchStudent() {
         System.out.print("Enter ID to search: ");
         int id = sc.nextInt();
@@ -44,21 +59,21 @@ public class StudentService {
                 return;
             }
         }
+
         System.out.println("Student not found.");
     }
-
     public void deleteStudent() {
         System.out.print("Enter ID to delete: ");
         int id = sc.nextInt();
 
-        for (Student s : students) {
-            if (s.getId() == id) {
-                students.remove(s);
-                System.out.println("Student deleted.");
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getId() == id) {
+                students.remove(i);
+                System.out.println("Student deleted successfully.");
                 return;
             }
         }
+
         System.out.println("Student not found.");
     }
 }
-
